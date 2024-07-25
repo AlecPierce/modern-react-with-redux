b803f69b94057bbfccf9494c0e40db392c6e4ea3
Alec <alecpierce19@gmail.com>
2024-07-25 17:08:53 -0500

    finished section 11

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,126 +1,24 @@
-import Button from "./Button";
-import {
-  RiZzzFill,
-  RiAlertFill,
-  RiAlertLine,
-  RiAddCircleFill,
-  RiAddCircleLine,
-  RiCheckFill,
-} from "react-icons/ri";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div className="flex justify-center">
-      <div>
-        <Button>
-          <RiZzzFill />
-          Plain
-        </Button>
-        <Button rounded>
-          <RiZzzFill />
-          Plain
-        </Button>
-        <Button rounded outline>
-          <RiZzzFill />
-          Plain
-        </Button>
-        <Button outline>
-          <RiZzzFill />
-          Plain
-        </Button>
-      </div>
-      <div>
-        <Button primary>
-          <RiAddCircleFill />
-          Primary
-        </Button>
-        <Button primary rounded>
-          <RiAddCircleFill />
-          Primary
-        </Button>
-        <Button primary rounded outline>
-          <RiAddCircleFill />
-          Primary
-        </Button>
-        <Button primary outline>
-          <RiAddCircleFill />
-          Primary
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          <RiAddCircleLine />
-          Secondary
-        </Button>
-        <Button secondary rounded>
-          <RiAddCircleLine />
-          Secondary
-        </Button>
-        <Button secondary rounded outline>
-          <RiAddCircleLine />
-          Secondary
-        </Button>
-        <Button secondary outline>
-          <RiAddCircleLine />
-          Secondary
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          <RiCheckFill />
-          Success
-        </Button>
-        <Button success rounded>
-          <RiCheckFill />
-          Success
-        </Button>
-        <Button success rounded outline>
-          <RiCheckFill />
-          Success
-        </Button>
-        <Button success outline>
-          <RiCheckFill />
-          Success
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <RiAlertLine />
-          Warning
-        </Button>
-        <Button warning rounded>
-          <RiAlertLine />
-          Warning
-        </Button>
-        <Button warning rounded outline>
-          <RiAlertLine />
-          Warning
-        </Button>
-        <Button warning outline>
-          <RiAlertLine />
-          Warning
-        </Button>
-      </div>
-      <div>
-        <Button danger>
-          <RiAlertFill />
-          Danger
-        </Button>
-        <Button danger rounded>
-          <RiAlertFill />
-          Danger
-        </Button>
-        <Button danger rounded outline>
-          <RiAlertFill />
-          Danger
-        </Button>
-        <Button danger outline>
-          <RiAlertFill />
-          Danger
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: 1,
+      label: "What is React?",
+      content: "React is a front end JavaScript framework",
+    },
+    {
+      id: 2,
+      label: "Why use React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      id: 3,
+      label: "How do you use React?",
+      content: "You use React by creating components",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
